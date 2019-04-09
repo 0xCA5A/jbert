e567de5cc08eb516a3fc57dc6fea4b0b137434d9
Samuel Casa <samuelcasa9@gmail.com>
2019-04-09 23:38:30 +0200

    Improve deployment
    Signed-off-by: Samuel Casa <samuelcasa9@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 APP_ARCHIVE=
-TARGET_DIR="/home/pi/"
+PROJECT_NAME="jbert"
+TARGET_DIR="/home/pi"
 TARGET_HOST=
 TARGET_USER="pi"
 
@@ -27,8 +28,35 @@ if [[ ! -f ${APP_ARCHIVE} ]]; then
     exit 1;
 fi
 
-echo -e "[i] Copy and extract application package"
-scp ${APP_ARCHIVE} ${TARGET_USER}@${TARGET_HOST}:${TARGET_DIR}
-ssh ${TARGET_USER}@${TARGET_HOST} "tar xf ${TARGET_DIR}/*.tgz -C ${TARGET_DIR}"
+
+echo -e "[i] Remove old application data on target"
+ssh "${TARGET_USER}@${TARGET_HOST}" "sudo rm -vrf ${TARGET_DIR}/${PROJECT_NAME}*"
+
+
+echo -e "[i] Copy application package"
+scp "${APP_ARCHIVE}" "${TARGET_USER}@${TARGET_HOST}:${TARGET_DIR}"
+
+
+echo -e "[i] Extract application package"
+ssh "${TARGET_USER}@${TARGET_HOST}" "tar -vxf ${TARGET_DIR}/*.tgz -C ${TARGET_DIR}"
+
+
+echo -e "[i] Create symlink"
+ssh "${TARGET_USER}@${TARGET_HOST}" "find ${TARGET_DIR} -type d -name \"${PROJECT_NAME}*\" -exec ln -s {} ${TARGET_DIR}/${PROJECT_NAME} ';' "
+
+
+echo -e "[i] Copy systemd service file"
+scp systemd/${PROJECT_NAME}.service ${TARGET_USER}@${TARGET_HOST}:/tmp
+ssh "${TARGET_USER}@${TARGET_HOST}" "sudo mv /tmp/${PROJECT_NAME}.service* /etc/systemd/system"
+
+
+echo -e "[i] Refresh systemd config"
+ssh "${TARGET_USER}@${TARGET_HOST}" "sudo systemctl enable ${PROJECT_NAME}"
+ssh "${TARGET_USER}@${TARGET_HOST}" "sudo systemctl daemon-reload"
+
+
+echo -e "[i] Restart the jbert application"
+ssh "${TARGET_USER}@${TARGET_HOST}" "sudo systemctl restart ${PROJECT_NAME}"
+
 
 echo -e "[i] Done"
