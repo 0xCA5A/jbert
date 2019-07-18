f4b269ddca34d232d94861287949a396c837d689
Samuel Casa <samuelcasa9@gmail.com>
2019-07-18 23:30:34 +0200

    Add RPi bootstrap ansible script
    Signed-off-by: Samuel Casa <samuelcasa9@gmail.com>

---
+++ bootstrap.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+TARGET_HOST=
+REMOTE_USER=pi
+
+
+if [[ $# -ne 1 ]]; then
+    echo "[!] Define a target host"
+    exit 1;
+fi
+TARGET_HOST=$1
+
+
+echo -e "[i] Create a Ansible hosts file"
+echo -e "[jberts]\n${TARGET_HOST}" > ansible/hosts
+cat ansible/hosts
+
+echo -e "[i] Run Ansible playbook"
+cd ansible
+time ansible-playbook bootstrap.yml -i hosts -u ${REMOTE_USER}
+cd -
+
+echo -e "[i] Remove generated ansible hosts file"
+rm -v ansible/hosts
+
+
+echo -e "[i] Done"
