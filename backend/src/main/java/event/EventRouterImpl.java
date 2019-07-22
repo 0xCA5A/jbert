a36132098820df7dbcca9295d4ca6a24b5db29a7
Samuel Casa <samuelcasa9@gmail.com>
2019-07-22 23:05:31 +0200

    Adjust event routing to new prototype hardware
    Signed-off-by: Samuel Casa <samuelcasa9@gmail.com>

--- backend/src/main/java/event/EventRouterImpl.java
+++ backend/src/main/java/event/EventRouterImpl.java
@@ -3,16 +3,14 @@ package event;
 import gpio.GpioService;
 import gpio.PlayNextTrackAction;
 import gpio.PlayPauseAction;
-import gpio.PlayPreviousTrackAction;
+import gpio.PlaylistLoadAction;
 import gpio.VolumeDownAction;
 import gpio.VolumeUpAction;
 import mpd.MpdService;
 import rfid.RfidService;
-import rfid.RfidTagDetectionListener;
+import rfid.RfidTagUid;
 
 import javax.inject.Inject;
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class EventRouterImpl implements EventRouter {
@@ -34,22 +32,29 @@ public class EventRouterImpl implements EventRouter {
     }
 
     private void configureGpiListener() {
-        gpioService.registerGpiListener("GPIO 21", new PlayPauseAction(mpdService));
-        gpioService.registerGpiListener("GPIO 3", new VolumeUpAction(mpdService));
-        gpioService.registerGpiListener("GPIO 5", new VolumeDownAction(mpdService));
-        gpioService.registerGpiListener("GPIO 27", new PlayNextTrackAction(mpdService));
-        gpioService.registerGpiListener("GPIO 4", new PlayPreviousTrackAction(mpdService));
+        gpioService.registerGpiListener("GPIO 4", new VolumeDownAction(mpdService)); // bcm 23
+        gpioService.registerGpiListener("GPIO 27", new VolumeUpAction(mpdService)); // bcm 16
+
+        gpioService.registerGpiListener("GPIO 21", new PlayPauseAction(mpdService)); // bcm 5
+        gpioService.registerGpiListener("GPIO 22", new PlayNextTrackAction(mpdService)); // bcm 6
+
+        gpioService.registerGpiListener("GPIO 3", new PlaylistLoadAction(mpdService, "red")); // bcm 22, red jbert button
+        gpioService.registerGpiListener("GPIO 23", new PlaylistLoadAction(mpdService, "black")); // bcm 13, black jbert button
+        gpioService.registerGpiListener("GPIO 7", new PlaylistLoadAction(mpdService, "green")); // bcm 4, blue jbert button
+        gpioService.registerGpiListener("GPIO 25", new PlaylistLoadAction(mpdService, "blue")); // bcm 26, blue jbert button
+        gpioService.registerGpiListener("GPIO 26", new PlaylistLoadAction(mpdService, "yellow")); // bcm 12, blue jbert button
     }
 
     private void configureRfidListener() {
-        List<RfidTagDetectionListener> rfidListeners = new ArrayList<>();
+        registerRfidServiceListener(new RfidTagUid("55-68-00-D2-EF"), "red");
+        registerRfidServiceListener(new RfidTagUid("FA-00-9C-73-15"), "black");
+        registerRfidServiceListener(new RfidTagUid("E5-EA-00-D2-DD"), "green");
+        registerRfidServiceListener(new RfidTagUid("F5-BA-4E-D3-D2"), "blue");
+        registerRfidServiceListener(new RfidTagUid("F5-82-FE-D1-58"), "yellow");
+    }
 
-        PlaylistChangeAction playListChangeAction = new PlaylistChangeAction(mpdService);
-        rfidListeners.add(playListChangeAction);
-
-        // Add all created listeners to the RfidTagDetector object
-        for (RfidTagDetectionListener rfidListener : rfidListeners) {
-            rfidService.addListener(rfidListener);
-        }
+    private void registerRfidServiceListener(RfidTagUid rfidTagUid, String playlist) {
+        PlaylistChangeAction playListChangeAction = new PlaylistChangeAction(mpdService, rfidTagUid, playlist);
+        rfidService.addListener(playListChangeAction);
     }
 }
--- backend/src/main/java/event/PlaylistChangeAction.java
+++ backend/src/main/java/event/PlaylistChangeAction.java
@@ -4,43 +4,23 @@ package event;
 import mpd.MpdService;
 import rfid.RfidTagDetectionListener;
 import rfid.RfidTagUid;
-import util.LogHelper;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.logging.Logger;
 
 public class PlaylistChangeAction extends RfidTagDetectionListener {
-    // Static config
-    private final static Map<String, String> playListMap;
-    private static final String redTagUid = "55-68-00-D2-EF";
-    private static final String redTagPlaylistName = "red";
-    private static final String blueTagUid = "F5-BA-4E-D3-D2";
-    private static final String blueTagPlaylistName = "blue";
-    private static final String greenTagUid = "E5-EA-00-D2-DD";
-    private static final String greenTagPlaylistName = "green";
-    private static Logger logger = LogHelper.getLogger(PlaylistChangeAction.class.getName());
-
-    static {
-        playListMap = new HashMap<>();
-        playListMap.put(redTagUid, redTagPlaylistName);
-        playListMap.put(blueTagUid, blueTagPlaylistName);
-        playListMap.put(greenTagUid, greenTagPlaylistName);
-    }
 
     private final MpdService mpdService;
+    private final RfidTagUid rfidTagUid;
+    private final String playlist;
 
-    public PlaylistChangeAction(MpdService mpdService) {
+    public PlaylistChangeAction(MpdService mpdService, RfidTagUid rfidTagUid, String playlist) {
         this.mpdService = mpdService;
+        this.rfidTagUid = rfidTagUid;
+        this.playlist = playlist;
     }
 
     @Override
     public void rfidTagAction(RfidTagUid rfidTagUid) {
-        final String key = rfidTagUid.toString();
-        if (playListMap.containsKey(key)) {
-            mpdService.loadPlaylist(playListMap.get(key));
-        } else {
-            logger.warning(String.format("No action for unsupported RFID Tag UID '%s'", rfidTagUid.toString()));
+        if (this.rfidTagUid.equals(rfidTagUid)) {
+            mpdService.loadPlaylist(playlist);
         }
     }
 }
