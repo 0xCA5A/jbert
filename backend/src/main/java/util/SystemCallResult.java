2b6c9aed18c41569c2accb55335dec9e1883b373
Samuel Casa <samuelcasa9@gmail.com>
2019-10-13 15:52:13 +0200

    Rework volume event handling
    Signed-off-by: Samuel Casa <samuelcasa9@gmail.com>

--- backend/src/main/java/util/SystemCallResult.java
+++ backend/src/main/java/util/SystemCallResult.java
@@ -1,13 +1,30 @@
 package util;
 
-class SystemCallResult {
-    String stdOut;
-    String stdErr;
-    int exitCode;
+public class SystemCallResult {
+    private final String stdOut;
+    private final String stdErr;
+    private final int exitCode;
 
     SystemCallResult(int exitCode, String stdOut, String stdErr) {
         this.exitCode = exitCode;
         this.stdOut = stdOut;
         this.stdErr = stdErr;
+    }
+
+    int getExitCode() {
+        return this.exitCode;
+    }
+
+    String getStdOut() {
+        return this.stdOut;
+    }
+
+    String getStdErr() {
+        return this.stdErr;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("{exitCode: %d, stdOut: %s, stdErr: %s}", exitCode, stdOut, stdErr);
     }
 }
